a4abb81362157eae5335a8ff0955744a7c4b1c0c
Paulin Shek <pshek@scottlogic.com>
2016-08-11 14:26:17 +0100

    Start from spring hello world tutorial

---
+++ src/main/java/library_project/controllers/BooksController.java
@@ -0,0 +1,21 @@
+package library_project.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import library_project.models.Book;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Book greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Book(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
---
+++ src/main/java/library_project/models/Book.java
@@ -0,0 +1,20 @@
+package library_project.models;
+
+public class Book {
+
+    private final long id;
+    private final String content;
+
+    public Book(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
