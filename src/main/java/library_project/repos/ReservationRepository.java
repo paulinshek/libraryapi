2ba9e25d3067eb3412898bb99e64a4254db2dd71
Paulin Shek <pshek@scottlogic.com>
2016-08-12 17:30:16 +0100

    Start to implement ReservationRepository

--- src/main/java/library_project/repos/ReservationRepository.java
+++ src/main/java/library_project/repos/ReservationRepository.java
@@ -1,38 +1,83 @@
 package library_project.repos;
 
+import library_project.models.Book;
 import library_project.models.Reservation;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by pshek on 12/08/2016.
  */
 public class ReservationRepository implements Repository<Reservation> {
     private List<Reservation> allReservations;
+    private Repository<Book> allBooks;
 
     public ReservationRepository() {
-        allReservations = new ArrayList<Reservation>();
+        allReservations = new ArrayList<>();
+        allBooks = new FilledRepository();
     }
 
     @Override
-    public Reservation get(int id) throws NoEntityException {
+    public Reservation get(int reservationId) throws NoEntityException {
         return null;
     }
 
     @Override
     public Iterator<Reservation> getAll() {
-        return null;
+        return allReservations.iterator();
     }
 
     @Override
-    public void add(Reservation entity) {
+    // TODO I WAS HERE LAST
+    public void add(Reservation reservation) {
+        // check that the book is not out
+        if (findReservations(reservation.getBookId()).size() == 0) {
+            allReservations.add(reservation);
+        } else {
+            throw new Exception();
+        }
 
     }
 
     @Override
     public void remove(int id) throws NoEntityException {
 
+    }
+
+    public Iterable<Book> getAllAvailableBooks(){
+        return null;
+    }
+
+    public Iterable<Book> getAllUnavailableBooks() {
+        return null;
+    }
+
+    public Iterable<Book> getAllLateBooks() {
+        return null;
+    }
+
+    // TODO: should this be static?
+    private Iterator<Integer> findBooks(Iterator<Reservation> reservations) {
+        Set<Integer> foundBooks = new HashSet<>();
+
+        while (reservations.hasNext()) {
+            foundBooks.add(reservations.next().getBookId());
+        }
+        return foundBooks.iterator();
+    }
+
+    private Iterator<Integer> findReservations(int bookId) {
+        List<Integer> foundReservations = new ArrayList<Integer>();
+        Iterator<Reservation> reservationIterator = getAll();
+        Reservation currReservation = null;
+
+        while (reservationIterator.hasNext()){
+            currReservation = reservationIterator.next();
+            if (currReservation.getReservationId() == bookId){
+                foundReservations.add(currReservation.getBookId());
+            }
+        }
+
+        return foundReservations.iterator();
     }
 }
