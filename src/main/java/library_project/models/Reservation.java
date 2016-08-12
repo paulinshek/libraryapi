b5ca52d496d95416e837213229350f8c33772b07
Paulin Shek <pshek@scottlogic.com>
2016-08-12 17:29:44 +0100

    Implement reservation class

--- src/main/java/library_project/models/Reservation.java
+++ src/main/java/library_project/models/Reservation.java
@@ -1,15 +1,43 @@
 package library_project.models;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
  * Created by pshek on 12/08/2016.
  */
 public class Reservation {
-    private Book book;
-    private Date startDate;
-    private Date endDate;
+    private int reservationId;
+    private int bookId;
+    private Calendar startDate;
+    private Calendar endDate;
     private boolean out;
 
+    public Reservation(int reservationId, int bookId) {
+        startDate = Calendar.getInstance();
 
+        endDate = Calendar.getInstance();
+        endDate.add(Calendar.DAY_OF_YEAR, 14);
+
+        this.reservationId = reservationId;
+        this.bookId = bookId;
+
+        out = true;
+    }
+
+    public void returnBook() {
+        out = false;
+    }
+
+    public boolean isLate() {
+        return Calendar.getInstance().compareTo(endDate) > 0;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
 }
