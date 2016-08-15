572fb7fd67d4caf10bc8eb6b6c2577ddea11baeb
Paulin Shek <pshek@scottlogic.com>
2016-08-15 12:26:58 +0100

    Pass through controller methods

--- src/main/java/library_project/controllers/LibraryController.java
+++ src/main/java/library_project/controllers/LibraryController.java
@@ -1,35 +1,45 @@
 package library_project.controllers;
 
 import library_project.models.Book;
+import library_project.models.BookIsOutException;
+import library_project.models.Library;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
 
 /**
  * Created by pshek on 12/08/2016.
  */
+@RestController
+@RequestMapping("library")
 public class LibraryController {
+    private Library library;
 
-    public Iterable<Book> getAllAvailableBooks(){
-        return null;
+    public LibraryController() {
+        library = new Library();
+    }
+    public Iterator<Book> getAllAvailableBooks(){
+        return library.getAllAvailableBooks();
     }
 
-    public Iterable<Book> getAllUnavailableBooks() {
-        return null;
-    }
+    public Iterator<Book> getAllUnavailableBooks() { return library.getAllUnavailableBooks(); }
 
-    public Iterable<Book> getAllLateBooks() {
-        return null;
+    public Iterator<Book> getAllLateBooks() {
+        return library.getAllLateBooks();
     }
 
     // try to get a book out for 2 week period
-    public void requestBook(int id) {
-
+    public void requestBook(int bookId) throws BookIsOutException {
+        library.requestBook(bookId);
     }
 
-    public boolean isOut(int id) {
-        return true;
+    public boolean isOut(int bookId) {
+        return library.isOut(bookId);
     }
 
-   public void returnBook(int id) {
-
+   public void returnBook(int reservationId) {
+        library.returnBook(reservationId);
    }
 
 }
