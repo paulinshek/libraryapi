e1ef179099b50911bdb52fa56beac9271fa26f1c
Paulin Shek <pshek@scottlogic.com>
2016-08-16 12:21:24 +0100

    Change API to use plural nouns

--- src/main/java/library_project/controllers/LibraryController.java
+++ src/main/java/library_project/controllers/LibraryController.java
@@ -46,7 +46,7 @@ public class LibraryController {
     /*
      * Gets the status of a book
      */
-    @RequestMapping(value="/reservation/status/{bookId}", method=RequestMethod.GET)
+    @RequestMapping(value="/reservations/status/{bookId}", method=RequestMethod.GET)
     public String isOut(@PathVariable("bookId") int bookId) {
         if (library.isOut(bookId)) return "Out";
         return "In";
@@ -55,7 +55,7 @@ public class LibraryController {
     /*
      * Return a book with reservationId
      */
-    @RequestMapping(value="/reservation/{reservationId}", method=RequestMethod.DELETE)
+    @RequestMapping(value="/reservations/{reservationId}", method=RequestMethod.DELETE)
     public String returnBook(@PathVariable("reservationId") int reservationId) {
         library.returnBook(reservationId);
         return "Book has now been returned";
@@ -64,7 +64,7 @@ public class LibraryController {
     /*
      * Reserve a book with bookId, returns the reservationId
      */
-    @RequestMapping(value="/reservation/book/{bookId}", method=RequestMethod.PUT)
+    @RequestMapping(value="/reservations/book/{bookId}", method=RequestMethod.PUT)
     public int requestBook(@PathVariable("bookId") int bookId) throws BookIsOutException {
         return library.requestBook(bookId);
     }
@@ -72,7 +72,7 @@ public class LibraryController {
     /*
      * List all reservations
      */
-    @RequestMapping(value="/reservation", method=RequestMethod.GET)
+    @RequestMapping(value="/reservations", method=RequestMethod.GET)
     public Iterator<Reservation> getAllReservations() {
         return library.getAllReservations();
     }
