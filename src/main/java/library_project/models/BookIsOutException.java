a57acfa075d10fd29ac96905861eb9747bc74dad
Paulin Shek <pshek@scottlogic.com>
2016-08-16 15:20:29 +0100

    Handle exception in spring

--- src/main/java/library_project/models/BookIsOutException.java
+++ src/main/java/library_project/models/BookIsOutException.java
@@ -1,7 +1,11 @@
 package library_project.models;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by pshek on 15/08/2016.
  */
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason = "Book is already out")
 public class BookIsOutException extends Exception {
 }
