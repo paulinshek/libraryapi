27507cec0a77a56e8b1ab34a958ae9dc436bf6fa
Paulin Shek <pshek@scottlogic.com>
2016-08-17 15:27:31 +0100

    Fill out methods in DatabaseConnector

--- src/main/java/library_project/databasetools/DatabaseConnector.java
+++ src/main/java/library_project/databasetools/DatabaseConnector.java
@@ -1,5 +1,9 @@
 package library_project.databasetools;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
@@ -11,16 +15,16 @@ public class DatabaseConnector {
     String dburl;
     Properties connectionProps;
 
-    public void setDburl(String dbUrl){
-
+    public void setDburl(String dburl){
+        this.dburl = dburl;
     }
 
-    public void setUsername(String username){
-
+    public void setUser(String user){
+        connectionProps.put("user", user);
     }
 
-    public void setPassword(String username){
-        
+    public void setPassword(String password){
+        connectionProps.put("password", password);
     }
 
     public Connection getConnection(){
