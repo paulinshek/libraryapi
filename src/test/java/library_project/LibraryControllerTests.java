44fb3bfbfe8481a47f0be8d5e81f645c9f5ce87d
Paulin Shek <pshek@scottlogic.com>
2016-08-18 16:10:12 +0100

    Write simple test

--- src/test/java/library_project/LibraryControllerTests.java
+++ src/test/java/library_project/LibraryControllerTests.java
@@ -1,7 +1,42 @@
 package library_project;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  * Created by pshek on 18/08/2016.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
 public class LibraryControllerTests {
+    @Autowired
+    private WebApplicationContext ctx;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+    @Test
+    public void tryMe() throws Exception {
+
+        this.mockMvc.perform(get("/api/reservations"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
